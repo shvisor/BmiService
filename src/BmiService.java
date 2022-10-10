59db1859ac12e7e301d0a3b32e466cf05d4aea56
Max Shvagerus <mshvagerus@gmail.com>
2022-10-10 15:43:11 +0300

    add comment

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double calculate(double w, double h) {
+    public double calculate(double w, double h) {  /* тип double, чтоб в промежуточных расчетах не было округления иначе результат не бъется */
         double count;
         count = w / Math.pow(h / 100, 2);          /* ИМТ = вес / рост ^ 2 (рост в метрах) */
-        long result = Math.round(count);
+        long result = Math.round(count);           /* на тип int редактор ругался, предлагая тип long */
         return result;
     }
 }
