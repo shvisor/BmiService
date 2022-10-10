b074f16881a2bff6dde6ffbc1e0f641ca225f070
Max Shvagerus <mshvagerus@gmail.com>
2022-10-10 15:59:24 +0300

    rounding

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double calculate(double w, double h) {  /* тип double, чтоб в промежуточных расчетах не было округления иначе результат не бъется */
+    public double calculate(double h, double w) {       /* тип double, чтоб в промежуточных расчетах не было округления иначе результат улетает */
         double count;
-        count = w / Math.pow(h / 100, 2);          /* ИМТ = вес / рост ^ 2 (рост в метрах) */
-        long result = Math.round(count);           /* на тип int редактор ругался, предлагая тип long */
+        count = w / Math.pow(h / 100, 2);               /* ИМТ = вес / рост ^ 2 (рост в метрах) */
+        double result = Math.round(count * 10d) / 10d;  /* округление до 1 знака после запятой */
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 102;                                    /* вес в килограммах */
-        int height = 182;                                    /* рост в сантиметрах */
-        double bmi = service.calculate(weight, height);
+        int height = 182;                                              /* рост в сантиметрах */
+        int weight = 102;                                              /* вес в килограммах */
+        double bmi = service.calculate(height, weight);
         System.out.println("Индекс массы тела: " + bmi + " кг/кв.м");
     }
 }
