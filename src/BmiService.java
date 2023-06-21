e35a5b91155b26067b485255c9ad7212e082e287
Max Shvagerus <mshvagerus@gmail.com>
2023-06-21 14:39:13 +0300

    index

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
-    public double calculate(double h, double w) {       /* тип double, чтоб в промежуточных расчетах не было округления иначе результат улетает */
-        double count;
-        count = w / Math.pow(h / 100, 2);               /* ИМТ = вес / рост ^ 2 (рост в метрах) */
-        double result = Math.round(count * 10d) / 10d;  /* округление до 1 знака после запятой */
-        return result;
+    public double calculate(double height, int weight) {       /* тип double, чтоб в промежуточных расчетах не было округления иначе результат улетает */
+        double index = weight / Math.pow(height, 2);               /* ИМТ = вес / рост ^ 2 (рост в метрах) */
+        return (int) index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int height = 182;                                              /* рост в сантиметрах */
-        int weight = 102;                                              /* вес в килограммах */
-        double bmi = service.calculate(height, weight);
-        System.out.println("Индекс массы тела: " + bmi + " кг/кв.м");
+        double heightMeter = 1.82;                                              /* рост в сантиметрах */
+        int weightKilo = 102;                                              /* вес в килограммах */
+        double index = service.calculate(heightMeter, weightKilo);
+        System.out.println("Индекс массы тела: " + index + " кг/кв.м");
     }
 }
