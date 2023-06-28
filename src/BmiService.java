0b917ae794ccd88adee6e4bc0e14afc5b29c6856
Max Shvagerus <mshvagerus@gmail.com>
2023-06-28 05:45:36 +0300

    add java doc

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,12 @@
 public class BmiService {
-    public double calculate(double height, int weight) {       /* тип double, чтоб в промежуточных расчетах не было округления иначе результат улетает */
-        double index = weight / Math.pow(height, 2);               /* ИМТ = вес / рост ^ 2 (рост в метрах) */
+    /**
+     * Расчет индекса массы тела
+     * @param height рост в метрах. Тип double, чтоб в промежуточных расчетах не было округления иначе результат улетает
+     * @param weight вес в килограммах
+     * @return индекс массы тела приведенный к типу int
+     */
+    public double calculate(double height, int weight) {
+        double index = weight / Math.pow(height, 2);
         return (int) index;
     }
 }
